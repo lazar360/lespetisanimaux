c2b68266308f9c388ba568c1b60f54d3c7f85c55
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-09 18:30:22 +0200

    create navbar

---
+++ pages/Commons/menu.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark perso_size20">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle perso_ColorRoseMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          L'asso
+        </a>
+        <div class="dropdown-menu " aria-labelledby="navbarDropdown">
+          <a class="dropdown-item perso_ColorRoseMenu" href="#">Action</a>
+          <a class="dropdown-item perso_ColorRoseMenu" href="#">Another action</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle perso_ColorOrangeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Pensionnaires
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item perso_ColorOrangeMenu" href="#">Action</a>
+          <a class="dropdown-item perso_ColorOrangeMenu" href="#">Another action</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle perso_ColorVertMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Actus
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item perso_ColorVertMenu" href="#">Action</a>
+          <a class="dropdown-item perso_ColorVertMenu" href="#">Another action</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle perso_ColorRougeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Conseils
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item perso_ColorRougeMenu" href="#">Action</a>
+          <a class="dropdown-item perso_ColorRougeMenu" href="#">Another action</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle perso_ColorBleuMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Contacts
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item perso_ColorBleuMenu" href="#">Action</a>
+          <a class="dropdown-item perso_ColorBleuMenu" href="#">Another action</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
