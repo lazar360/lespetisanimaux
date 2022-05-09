eda269fe425a1b8e87797048f9b03dab04e945de
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-09 12:11:44 +0200

    maj index and css

--- pages/index.php
+++ pages/index.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link href="../css/main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Copse&family=Fredoka+One&display=swap" rel="stylesheet">
     <title>Les Petits Animaux</title>
   </head>
   <body>
-    <h1>COUCOU</h1>
 
+  <p>Coucou c'est u paragraphe</p>
+  <h1 class="perso_policeTitre">C'est un titre</h1>
     
 
     
