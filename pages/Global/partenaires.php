82437453f0b061318497f1bc6afae4407464890c
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-10 18:00:24 +0200

    add association page create partenaires page

--- pages/Global/partenaires.php
+++ pages/Global/partenaires.php
@@ -1,3 +1,13 @@
 <?php include("../Commons/header.php") ?>
-Page des Partenaires de l'association
+
+<h2 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">Les partenaires</h2>
+<div class="card" style="width: 18rem;">
+  <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top" alt="updp-logo">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
 <?php include("../Commons/footer.php") ?>
