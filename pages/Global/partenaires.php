309ea2abe22f004fde1a8918159012350acc43ee
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-10 18:12:31 +0200

    update partenaires page

--- pages/Global/partenaires.php
+++ pages/Global/partenaires.php
@@ -1,13 +1,63 @@
 <?php include("../Commons/header.php") ?>
 
 <h2 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">Les partenaires</h2>
-<div class="card" style="width: 18rem;">
-  <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top" alt="updp-logo">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+
+<div class="row no-gutters">
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+    <div class="card col-auto mx-auto mt-2" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="updp-logo">
+        <div class="card-body text-center">
+            <h5 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id itaque cumque possimus temporibus! </p>
+            <a href="#" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+
+
 </div>
 
 <?php include("../Commons/footer.php") ?>
