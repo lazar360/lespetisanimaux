a3ca5d55769e7b658034615cd37c914f34e38afa
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-10 17:18:01 +0200

    commit

--- pages/Global/index.php
+++ pages/Global/index.php
@@ -1,3 +1,102 @@
 <?php include("../Commons/header.php") ?>
-INDEX
+
+<h2 class="text-center mt-3 perso_ColorRoseMenu perso_policeTitre perso_textShadow">Ils ont besoin de vous !</h2>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active bg-dark"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="bg-dark"></li>
+    <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2" class="bg-dark"></li> -->
+  </ol>
+
+  <div class="carousel-inner">
+    <!-- Début d'un item -->
+    <div class="carousel-item active">
+      <div class="row no-gutters border rounded overflow-hidden mb-4 perso_bgRose">
+        <div class="col-12 col-md-auto text-center">
+          <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" style="height: 250px;" alt="Photo de Framboise">
+        </div>
+        <div class="col-12 col-md">
+          <div class="col p-4 d-flex flex-column position-static">
+            <h3 class="mb-0 perso_policeTitre perso_ColorRoseMenu perso_textShadow">Framboise</h3>
+            <div class="text-muted mb-1">02/2019</div>
+            <p class="perso_policeTexte" style="margin-bottom: 70px;">
+              Description de Framboise
+            </p>
+            <a href="#" class="btn btn-primary">Visiter ma page</a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Fin d'un item -->
+
+    <!-- Début d'un item -->
+    <div class="carousel-item">
+      <div class="row no-gutters border rounded overflow-hidden mb-4 perso_bgRose">
+        <div class="col-12 col-md-auto text-center">
+          <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" style="height: 250px;" alt="Photo de Framboise">
+        </div>
+        <div class="col-12 col-md">
+          <div class="col p-4 d-flex flex-column position-static">
+            <h3 class="mb-0 perso_policeTitre perso_ColorRoseMenu perso_textShadow">Framboise</h3>
+            <div>02/2019</div>
+            <p class="perso_policeTexte" style="margin-bottom: 70px;">
+              Description de Framboise
+            </p>
+            <a href="#" class="btn btn-primary">Visiter ma page</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Fin d'un item -->
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-6 mt-3">
+    <h2 class="text-center mt-3 perso_ColorVertMenu perso_policeTitre perso_textShadow">
+      <img src="../../sources/images/Autres/icones/journal.png" alt="logo news" /> Nouvelles des adoptés
+    </h2>
+  </div>
+
+  <div class="col-6 mt-3">
+    <h2 class="text-center mt-3 perso_ColorOrangeMenu perso_policeTitre perso_textShadow">
+      <img src="../../sources/images/Autres/icones/action.png" alt="logo news" />Evènements et actions
+    </h2>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-6">
+    <div class="row no-gutters border rounded mb-4">
+      <div class="col-auto d-none d-lg-block">
+        <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" style="height: 170px;" alt="photo de framboise" />
+      </div>
+      <div class="col p-3 d-flex flex-column position-static perso_bgGreen">
+        <h3 class="mb-0 mb-0 perso_policeTitre perso_ColorVertMenu perso_textShadow">Doyenne Chipie</h3>
+        <p class="perso_size12  mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit saepe quidem ipsam repudiandae culpa cumque doloremque, modi quam magni corporis sed, voluptatibus nihil eveniet ad perspiciatis asperiores, hic vero molestias!
+        </p>
+        <a href="#" class="btn btn-primary">Visiter ma page</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6">
+  <div class="row no-gutters border rounded mb-4">
+      <div class="col-auto d-none d-lg-block">
+        <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" style="height: 170px;" alt="photo de framboise" />
+      </div>
+      <div class="col p-3 d-flex flex-column position-static perso_bgOrange">
+        <h3 class="mb-0 mb-0 perso_policeTitre perso_ColorOrangeMenu perso_textShadow">Doyenne Chipie</h3>
+        <p class="perso_size12 mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit saepe quidem ipsam repudiandae culpa cumque doloremque, modi quam magni corporis sed, voluptatibus nihil eveniet ad perspiciatis asperiores, hic vero molestias!
+        </p>
+        <a href="#" class="btn btn-primary">Visiter ma page</a>
+      </div>
+    </div>
+  
+  </div>
+</div>
+
 <?php include("../Commons/footer.php") ?>
