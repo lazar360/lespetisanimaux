fdaa258681894841e6c2bd403a10c0637dc648fc
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-12 11:38:35 +0200

    create pensionnaire.php

---
+++ pages/Global/pensionnaire.php
@@ -0,0 +1,102 @@
+<?php include("../Commons/header.php"); ?>
+
+<?= (styleTitreNiveau1("Ils cherchent une famille", COLOR_PENSIONNAIRE)) ?>
+
+<div class="row no-gutters">
+    <div class="col-12 col-lg-6">
+        <div class="row border-dark rounded-lg m-2 align-items-center perso_bgRose" style="height: 200px;">
+            <div class="col p-2 text-center">
+                <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" class="img-thumbnail" alt="framboise" style="max-height: 180px;">
+            </div>
+            <div class="col-2 border-left border-right border-dark text-center">
+                <img src="../../sources/images/Autres/icones/chienOk.png" class="img-fluid" alt="chien ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/chatOk.png" class="img-fluid" alt="chat ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/babyOk.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
+
+            </div>
+            <div class="col-6 text-center">
+                <div class="perso_policeTitre perso_size20 mb-3">Framboise</div>
+                <div class="mb-2">Née : ++/++/++++</div>
+                <div class="my-3">
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">douce</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">calme</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-md-inline">joueuse</span>
+                </div>
+                <a href="animal.php" class="btn btn-primary">Visiter ma page</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-12 col-lg-6">
+        <div class="row border-dark rounded-lg m-2 align-items-center perso_bgRose" style="height: 200px;">
+            <div class="col p-2 text-center">
+                <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" class="img-thumbnail" alt="framboise" style="max-height: 180px;">
+            </div>
+            <div class="col-2 border-left border-right border-dark text-center">
+                <img src="../../sources/images/Autres/icones/chienOk.png" class="img-fluid" alt="chien ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/chatOk.png" class="img-fluid" alt="chat ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/babyOk.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
+            </div>
+            <div class="col-6 text-center">
+                <div class="perso_policeTitre perso_size20 mb-3">Framboise</div>
+                <div class="mb-2">Née : ++/++/++++</div>
+                <div class="my-3">
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">douce</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">calme</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-md-inline">joueuse</span>
+                </div>
+                <a href="animal.php" class="btn btn-primary">Visiter ma page</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-6">
+        <div class="row border-dark rounded-lg m-2 align-items-center perso_bgRose" style="height: 200px;">
+            <div class="col p-2 text-center">
+                <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" class="img-thumbnail" alt="framboise" style="max-height: 180px;">
+            </div>
+            <div class="col-2 border-left border-right border-dark text-center">
+                <img src="../../sources/images/Autres/icones/chienOk.png" class="img-fluid" alt="chien ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/chatOk.png" class="img-fluid" alt="chat ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/babyOk.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
+            </div>
+            <div class="col-6 text-center">
+                <div class="perso_policeTitre perso_size20 mb-3">Framboise</div>
+                <div class="mb-2">Née : ++/++/++++</div>
+                <div class="my-3">
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">douce</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">calme</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-md-inline">joueuse</span>
+                </div>
+                <a href="animal.php" class="btn btn-primary">Visiter ma page</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-6">
+        <div class="row border-dark rounded-lg m-2 align-items-center perso_bgRose" style="height: 200px;">
+            <div class="col p-2 text-center">
+                <img src="../../sources/images/Animaux/Chats/Framboise/Framboise.jpg" class="img-thumbnail" alt="framboise" style="max-height: 180px;">
+            </div>
+            <div class="col-2 border-left border-right border-dark text-center">
+                <img src="../../sources/images/Autres/icones/chienOk.png" class="img-fluid" alt="chien ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/chatOk.png" class="img-fluid" alt="chat ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/babyOk.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
+            </div>
+            <div class="col-6 text-center">
+                <div class="perso_policeTitre perso_size20 mb-3">Framboise</div>
+                <div class="mb-2">Née : ++/++/++++</div>
+                <div class="my-3">
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">douce</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">calme</span>
+                    <span class="badge badge-warning m-1 p-2 d-none d-md-inline">joueuse</span>
+                </div>
+                <a href="animal.php" class="btn btn-primary">Visiter ma page</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php include("../Commons/footer.php") ?>
