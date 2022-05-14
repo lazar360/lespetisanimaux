ffeb288a505988bc8084dac216a7680d5338266a
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-14 09:46:59 +0200

    update pensionnaire.php

--- pages/Global/test.php
+++ pages/Global/test.php
@@ -1,7 +1,4 @@
 <?php
-
-use LDAP\Result;
-
 require_once("../Global/pdo.php");
 include("../Commons/header.php");
 ?>
@@ -13,6 +10,8 @@ $stmt->execute();
 $animaux = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor();
 ?>
+
+<?= (styleTitreNiveau1("Ils cherchent une famille", COLOR_PENSIONNAIRE)) ?>
 
 <div class="row no-gutters">
     <?php foreach ($animaux as $animal) : ?>
@@ -32,30 +31,57 @@ $stmt->closeCursor();
         <div class="col-12 col-lg-6">
             <div class="row border-dark rounded-lg m-2 align-items-center perso_bgRose" style="height: 200px;">
                 <div class="col p-2 text-center">
-                    <img src="../../sources/images/Animaux/Chats/<?= $image['url_image'] ?>" class="img-thumbnail" alt="<?= $image['description_image'] ?>" style="max-height: 180px;">
+                    <img src="../../sources/images/Animaux/<?= $animal['type_animal'] ?>/<?= $image['url_image'] ?>" class="img-thumbnail" alt="<?= $image['description_image'] ?>" style="max-height: 180px;">
                 </div>
                 <div class="col-2 border-left border-right border-dark text-center">
-                    <img src="../../sources/images/Autres/icones/chienOk.png" class="img-fluid" alt="chien ok" style="width: 50px;">
-                    <img src="../../sources/images/Autres/icones/chatOk.png" class="img-fluid" alt="chat ok" style="width: 50px;">
-                    <img src="../../sources/images/Autres/icones/babyOk.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
+                <?php 
+                $iconeChien = "";
+                $iconeChat = "";
+                $iconeEnfant = "";
+                
+                if($animal['ami_chien'] === "oui") $iconeChien = "chienOk";
+                else if($animal['ami_chien'] === "non") $iconeChien = "chienBar";
+                else if ($animal['ami_chien'] === "N/A") $iconeChien = "chienQuest";
+                    
+                if($animal['ami_chat'] === "oui") $iconeChat = "chatOk";
+                else if($animal['ami_chat'] === "non") $iconeChat = "chatBar";
+                else if ($animal['ami_chat'] === "N/A") $iconeChat = "chatQuest";
+                
+                if($animal['ami_enfant'] === "oui") $iconeEnfant = "babyOk";
+                else if($animal['ami_enfant'] === "non") $iconeEnfant = "babyBar";
+                else if ($animal['ami_enfant'] === "N/A") $iconeEnfant = "babyQuest";                
+                ?>
+
+                <img src="../../sources/images/Autres/icones/<?=$iconeChien ?>.png" class="img-fluid" alt="chien ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/<?=$iconeChat ?>.png" class="img-fluid" alt="chat ok" style="width: 50px;">
+                <img src="../../sources/images/Autres/icones/<?=$iconeEnfant ?>.png" class="img-fluid" alt="bébé ok" style="width: 50px;">
 
                 </div>
                 <div class="col-6 text-center">
                     <div class="perso_policeTitre perso_size20 mb-3"><?= $animal['nom_animal'] ?></div>
                     <div class="mb-2">Née : <?= $animal['date_naissance_animal'] ?></div>
                     
-                    <!-- <div class="my-3">
-                        <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">douce</span>
-                        <span class="badge badge-warning m-1 p-2 d-none d-sm-inline">calme</span>
-                        <span class="badge badge-warning m-1 p-2 d-none d-md-inline">joueuse</span>
-                    </div> -->
-                    <!-- <a href="animal.php" class="btn btn-primary">Visiter ma page</a> -->
+                    <?php
+                        $stmt = $bdd->prepare( 'SELECT c.libelle_caractere 
+                        FROM caractere c 
+                        INNER JOIN dispose d ON c.id_caractere = d.id_caractere
+                        INNER JOIN animal a ON a.id_animal = d.id_animal
+                        WHERE a.id_animal = :idAnimal');
+                        $stmt->bindValue(":idAnimal", $animal['id_animal']);
+                        $stmt->execute();
+                        $caracteres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        $stmt->closeCursor();
+                    ?>
+
+                    <div class="my-3">
+                        <?php foreach($caracteres as $caractere) : ?>
+                        <span class="badge badge-pill badge-secondary"><?= $caractere['libelle_caractere'] ?></span>
+                        <?php endforeach; ?>
+                    </div>
+                    <a href="animal.php?idAnimal=<?=$animal['id_animal']?>" class="btn btn-primary">Visiter ma page</a>
                 </div>
             </div>
         </div>
     <?php endforeach; ?>
 </div>
-
-
-
 <?php include("../Commons/footer.php") ?>
